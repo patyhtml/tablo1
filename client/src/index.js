65b35ac980a4fbdf069f3384c65e016eb69c6853
Patrycja Gotszling <patrycjagotszling@MacBook-Pro-Patrycja.local>
2024-09-09 01:33:58 +0200

    backend trasy

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,19 @@ import App from './App';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+// fetch('http://localhost:3001')
+// .then(response => response.text()
+// )
+// .then(response =>{
+
+//   console.log(response)
+// })
+
+
+
+
+
+
 root.render(
   <StrictMode>
     <App />
--- server/app.js
+++ server/app.js
@@ -1,26 +1,26 @@
 const express = require('express');
-const pool = require('./config/db');  // Import połączenia do bazy danych PostgreSQL
+// const pool = require('./config/db');  // Import połączenia do bazy danych PostgreSQL
 const multer = require('multer');  // Import Multer do obsługi plików
 const fileController = require('./controllers/fileController');  // Import kontrolera do plików
-
+const cors = require('cors');
 const app = express();
 
 // Middleware do parsowania JSON (jeśli potrzebne do przetwarzania danych z żądań POST)
 app.use(express.json());
-
+app.use(cors());
 // Prosta trasa, która zwróci wiadomość
 app.get('/', (req, res) => {
     res.send('Witaj w moim backendzie Node.js!');
 });
 
 // Przykład zapytania do bazy danych
-pool.query('SELECT NOW()', (err, res) => {
-    if (err) {
-        console.error('Błąd połączenia z bazą danych:', err);
-    } else {
-        console.log('Połączono z bazą danych:', res.rows);
-    }
-});
+// pool.query('SELECT NOW()', (err, res) => {
+//     if (err) {
+//         console.error('Błąd połączenia z bazą danych:', err);
+//     } else {
+//         console.log('Połączono z bazą danych:', res.rows);
+//     }
+// });
 
 // Konfiguracja Multer do zapisywania plików w folderze 'uploads/'
 const upload = multer({ dest: 'uploads/' });  // Folder, gdzie będą zapisywane pliki
@@ -31,8 +31,20 @@ app.post('/api/files/upload', upload.single('file'), fileController.uploadFile);
 // Trasa do pobierania plików po ID
 app.get('/api/files/:id', fileController.getFileById);
 
+app.get('/api/boards');//lista boards user
+app.post('/api/boards'); // Dodawanie board for user
+app.get('/api/boards/:boardId');// detal board
+app.post('/api/boards/:boardId');// zapis edycji board
+app.delete('/api/boards/:boardId');// kasuje detal board
+
+
+
+
+
+
+
 // Uruchomienie serwera na porcie 3000
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Serwer działa na porcie ${PORT}`);
 });
